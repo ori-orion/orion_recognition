395fe1372123343c4d62295da92b9ad977bd0244
ascane <mandy00724@gmail.com>
2019-05-23 12:28:41 +0100

    Add a comment.

--- scripts/bbox_publisher_node.py
+++ scripts/bbox_publisher_node.py
@@ -9,7 +9,7 @@ def main():
     rospy.init_node('bbox_publisher_node')
     img_topic = "/hsrb/head_rgbd_sensor/rgb/image_rect_color"
     model_name = 'ssd_mobilenet_v1_coco_2018_01_28'
-    path_to_model = os.path.join('/home/chiaman/git/models/research/object_detection')
+    path_to_model = os.path.join('/home/chiaman/git/models/research/object_detection')  # TODO: No hardcoded location
     detect_pers = True
     detect_obj = True
     thresh = 0.4
