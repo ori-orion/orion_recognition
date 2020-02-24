30b0d8762878c68134f23ff4782b175afe946633
cscosten268 <54147617+cscosten268@users.noreply.github.com>
2020-02-24 10:37:22 +0000

    Update bbox_publisher_node.py

--- scripts/bbox_publisher_node.py
+++ scripts/bbox_publisher_node.py
@@ -8,12 +8,8 @@ from orion_recognition.bbox_publisher import BboxPublisher
 def main():
     rospy.init_node('bbox_publisher_node')
     img_topic = "/hsrb/head_rgbd_sensor/rgb/image_rect_color"
-    model_name = 'ssd_resnet50'
-    path_to_model = os.path.join('/home/ori/code/recognition/models-master/research/object_detection')  # TODO: No hardcoded location
-    detect_pers = True
-    detect_obj = True
-    thresh = 0.4
-    BboxPublisher(img_topic, model_name, path_to_model, detect_obj, detect_pers, thresh)
+    depth_topic = "/hsrb/head_rgbd_sensor/depth_registered/image_rect_raw"
+    BboxPublisher(img_topic, depth_topic)
     rospy.spin()
 
 
