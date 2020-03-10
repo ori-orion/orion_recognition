4490665e325b619dbfc3485df72ffe3ca0695713
fumie <fumie@localhost.localdomain>
2020-03-10 14:42:29 +0000

    added nms and new labels

--- src/orion_recognition/bbox_publisher.py
+++ src/orion_recognition/bbox_publisher.py
@@ -3,13 +3,11 @@
 import orion_recognition.object_detector
 import message_filters
 from orion_actions.msg import Detection, DetectionArray, Label
-import pdb
 import sys
 sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
 import cv2
 sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages')
 import numpy as np
-import os
 import rospy
 import std_msgs.msg
 from geometry_msgs.msg import Point
@@ -17,12 +15,19 @@ from sensor_msgs.msg import CameraInfo, Image
 from cv_bridge import CvBridge, CvBridgeError
 from orion_recognition.colornames import ColorNames
 import torchvision.transforms as transforms
+import torchvision.ops as ops
+import torch
 
 class BboxPublisher(object):
     def __init__(self, image_topic, depth_topic):
         self.detector = orion_recognition.object_detector.ObjectDetector()
         self.detector.eval()
-        # Subscribers        
+        # Label dictionary
+        f = open('coco_labels.txt')
+        self.label_dict = f.readlines()
+        f.close()
+
+        # Subscribers
         self.image_sub = message_filters.Subscriber(image_topic, Image, queue_size=100)
         self.depth_sub = message_filters.Subscriber(depth_topic, Image)
 
@@ -32,7 +37,7 @@ class BboxPublisher(object):
         #Publishers
         self.image_pub = rospy.Publisher('/vision/bbox_image', Image, queue_size=100)
         self.detections_pub = rospy.Publisher('/vision/bbox_detections', DetectionArray, queue_size=100)
-        
+
         #Image calibrator
         camera_info = rospy.wait_for_message(
             "/hsrb/head_rgbd_sensor/depth_registered/camera_info", CameraInfo)
@@ -44,8 +49,6 @@ class BboxPublisher(object):
         #Register a subscriber
         self.subscribers.registerCallback(self.callback)
 
-
-
     def callback(self, ros_image, depth_data):
         # get images from cv bridge
         image = self.bridge.imgmsg_to_cv2(ros_image, "bgr8")
@@ -54,42 +57,39 @@ class BboxPublisher(object):
 
         image_np = np.asarray(image)
         image_tensor = transforms.ToTensor()(image)
-        img_x = image_tensor.size()[2]
-        img_y = image_tensor.size()[1]
-        
+
         #apply model to image
         detections = self.detector([image_tensor])[0]
-        
+
         boxes = detections['boxes']
         labels = detections['labels']
         scores = detections['scores']
-        
+
         detections = []
+        boxes_nms = []
+        scores_nms = []
+
         for i in range(len(boxes)):
             box = boxes[i]
             label = labels[i]
             score = scores[i]
-            
-            #draw rectangle onto the image
-            top_left = (int(box[0]), int(box[1]))
-            bottom_right = (int(box[2]), int(box[3]))
-            cv2.rectangle(image, top_left, bottom_right, (255, 0, 0), 3)
-            
+
             # Dimensions of bounding box
             center_x = (box[0]+box[2])/2
             width = box[2]-box[0]
             center_y = (box[1]+box[3])/2
             height = box[3]-box[1]
-            
+
             # Get depth
             trim_depth = depth[int(box[0]):int(box[2]), int(box[1]):int(box[2])]
             valid = trim_depth[np.nonzero(trim_depth)]
-            
+
+            # Use depth to get position, and if depth is not valid, discard bounding box
             if valid.size != 0:
                 z = np.min(valid) * 1e-3
-                top_left_3d = np.array([top_left[0], top_left[1], 0])
+                top_left_3d = np.array([int(box[0]), int(box[1]), 0])
                 top_left_camera = np.dot(self._invK, top_left_3d)*z
-                bottom_right_3d = np.array([bottom_right[0], bottom_right[1], 0])
+                bottom_right_3d = np.array([int(box[2]), int(box[3]), 0])
                 bottom_right_camera = np.dot(self._invK, bottom_right_3d)*z
                 corner_to_corner = top_left_camera - bottom_right_camera
                 x_size = abs(corner_to_corner[0])
@@ -110,21 +110,37 @@ class BboxPublisher(object):
             RGB = np.mean(crop, axis=(0,1))
             RGB = (RGB[2], RGB[1], RGB[0])
             colour = ColorNames.findNearestOrionColorName(RGB)
-            
-            # create label
-            score_lbl = Label(str(label).encode('ascii','ignore'), np.float64(score))
-            
-            # create detection instance
-            detection = Detection(score_lbl, center_x, center_y, width, height, size, colour, obj[0], obj[1], obj[2])
-            
-            detections.append(detection)
 
-        
+            # create label
+            score_lbl = Label(str(self.label_dict[int(label)-1]).encode('ascii', 'ignore'),
+                              np.float64(score))
+
+            # create detection instance
+            detection = Detection(score_lbl, center_x, center_y, width, height,
+                                  size, colour, obj[0], obj[1], obj[2])
+
+            detections.append(detection)
+            boxes_nms.append(box)
+            scores_nms.append(float(score))
+
+        # Perform non-maximum suppression on boxes according to their intersection over union (IoU)
+        boxes_nms = torch.stack(boxes_nms)
+        scores_nms = torch.stack(scores_nms)
+        keep = ops.nms(boxes_nms, scores_nms, 0.5)
+
+        clean_detections = [detections[i] for i in keep]
+
+        #Draw bounding boxes of cleaned detections onto image
+        for j in keep:
+            top_left = (int(boxes_nms[j][0]), int(boxes_nms[j][1]))
+            bottom_right = (int(boxes_nms[j][2]), int(boxes_nms[j][3]))
+            cv2.rectangle(image, top_left, bottom_right, (255, 0, 0), 3)
+
         # Publish nodes
         try:
             h = std_msgs.msg.Header()
             h.stamp = rospy.Time.now()
-            self.detections_pub.publish(DetectionArray(h, detections))
+            self.detections_pub.publish(DetectionArray(h, clean_detections))
             self.image_pub.publish(self.bridge.cv2_to_imgmsg(image, "bgr8"))
         except CvBridgeError as e:
             print(e)
