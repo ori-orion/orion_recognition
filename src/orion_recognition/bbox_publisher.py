5280f22bfaddb0eda646f01fea0ee81f59db39ba
Chia-Man Hung <mandy00724@gmail.com>
2021-06-08 11:10:45 +0100

    Kinetic to melodic

--- src/orion_recognition/bbox_publisher.py
+++ src/orion_recognition/bbox_publisher.py
@@ -4,9 +4,9 @@ import orion_recognition.object_detector
 import message_filters
 from orion_actions.msg import Detection, DetectionArray, Label
 import sys
-sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+sys.path.remove('/opt/ros/melodic/lib/python2.7/dist-packages')
 import cv2
-sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages')
+sys.path.append('/opt/ros/melodic/lib/python2.7/dist-packages')
 import numpy as np
 import rospy
 import std_msgs.msg
