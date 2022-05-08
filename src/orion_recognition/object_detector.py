c11fc6dfcd970fbab5ef3f5d6f89085aec894a99
cscosten268 <ccosten.costen@gmail.com>
2022-05-08 20:58:02 +0100

    new functions train on data set and link to computer's camera to test the model

--- src/orion_recognition/object_detector.py
+++ src/orion_recognition/object_detector.py
@@ -7,6 +7,7 @@ import torch
 import torchvision.models as models
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
+import cv2
 
 from PIL import Image
 
@@ -28,6 +29,26 @@ class ObjectDetector(torch.nn.Module):
         y = [{k: v.cpu().detach().numpy() for k,v in y[i].items()} for i in range(len(y))]
         return y 
 
+    def train_on_data(self):
+        images = []
+        targets = []
+        labels_file = open('labels.txt', 'r')
+        labels = labels_file.read().split("\n")
+        for i in range(len(labels)):
+            image_dir = "./"+labels[i]+"/"
+            for image_location in os.listdir(image_dir):
+                image = Image.open(image_dir+image_location)
+                image_tensor = transforms.ToTensor()(image)
+                images.append(image_tensor)
+                size = image_tensor.size()
+                d  = {}
+                d['boxes'] = [[0, 0, size[1], size[2]]]
+                d['labels'] = [i+91]
+                targets.append(d)
+        self.model(images, targets)
+        torch.save(self.model.state_dict(), "model_weights.pth")
+        # model.load_state_dict(torch.load("model_weights.pth"))
+
 
 
     def detect_random(self):
@@ -46,12 +67,39 @@ class ObjectDetector(torch.nn.Module):
         output = self.model([test])
         return output
 
+    def detect_video(self):
+        self.model.eval()
+        cv2.namedWindow("preview")
+        vc = cv2.VideoCapture(0)
+
+        if vc.isOpened(): # try to get the first frame
+            rval, frame = vc.read()
+            image_tensor = transforms.ToTensor()(frame)
+        else:
+            rval = False
+
+        while rval:
+            detections = self.model([image_tensor])[0]
+            for detection, label in zip(detections['boxes'], detections['labels']):
+                cv2.rectangle(frame, (int(detection[0]), int(detection[1])), (int(detection[2]), int(detection[3])), (255, 0, 0), 3)
+                cv2.putText(frame, str(label), (int(detection[0]), int(detection[1])), cv2.FONT_HERSHEY_COMPLEX,0.5,(0,255,0),1)
+            cv2.imshow("preview", frame)
+            rval, frame = vc.read()
+            image_tensor = transforms.ToTensor()(frame)
+            key = cv2.waitKey(20)
+            if key == 27: # exit on ESC
+                break
+
+        vc.release()
+        cv2.destroyWindow("preview")
+
 
 if __name__ == '__main__':
     detector = ObjectDetector()
     print(detector.device)
-    #result = detector.detect_test()
-    result = detector.detect_random()
+    #result = detector.detect_video()
+    detector.train_on_data()
+    #result = detector.detect_random()
     #print(result[0]['boxes'])
     #print(result[0]['labels'])
     #print(result[0]['scores'])
