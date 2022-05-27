3681ec29d26d5f1fa876c45ba40bf77535b1051d
shuishida <ishidashu@gmail.com>
2022-05-27 17:04:08 +0100

    Fix to download script

--- scripts/download_athome_dataset.sh
+++ scripts/download_athome_dataset.sh
@@ -1,6 +1,7 @@
 # run from the orion_recognition root directory
 mkdir data
 cd data
+mkdir athome
 gdown https://drive.google.com/uc?id=11qrFBccj7tS3fW_3XAssQY1nAzhPnBPr
-tar -xf athome.tar.gz athome
+tar -xf athome.tar.gz -C athome
 cd ../
