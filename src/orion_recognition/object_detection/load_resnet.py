7a6872ee1bc5bc5cf3bdeddd1488728b67883197
shuishida <ishidashu@gmail.com>
2022-06-03 13:55:44 +0100

    Added finetuned model

--- src/orion_recognition/object_detection/load_resnet.py
+++ src/orion_recognition/object_detection/load_resnet.py
@@ -13,7 +13,7 @@ from orion_recognition.object_detection.utils import load_finetuned_resnet
 dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--checkpoint", help="path to checkpoint", default=os.path.join(dir_path, "finetuned", "resnet_epoch_0.pth"))
+parser.add_argument("--checkpoint", help="path to checkpoint", default=os.path.join(dir_path, "finetuned", "resnet_epoch_16.pth"))
 parser.add_argument("--n_classes", help="number of classes", type=int, default=180)
 
 args = parser.parse_args()
