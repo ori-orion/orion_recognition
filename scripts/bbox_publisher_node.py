1596fa47ef0eec43294ee14e646e2878000005db
Ricardo Cannizzaro <ricardo.cannizzaro@gmail.com>
2023-06-29 02:10:35 +0700

    Adding small changes to bbox_publisher node.

--- scripts/bbox_publisher_node.py
+++ scripts/bbox_publisher_node.py
@@ -9,8 +9,8 @@ from orion_recognition.bbox_publisher import BboxPublisher
 def main():
     print("bbox_publisher_node.py.main() begin.")
     rospy.init_node('bbox_publisher_node')
-    img_topic = "/hsrb/head_rgbd_sensor/rgb/image_rect_color";
-    depth_topic = "/hsrb/head_rgbd_sensor/depth_registered/image_rect_raw";
+    img_topic ="/hsrb/head_rgbd_sensor/rgb/image_rect_color"; #"camera/color/image_raw" 
+    depth_topic =  "/hsrb/head_rgbd_sensor/depth_registered/image_rect_raw"; #"/camera/aligned_depth_to_color/image_raw"   
     BboxPublisher(img_topic, depth_topic);
     print("bbox_publisher_node.py.main() end.")
     rospy.spin()
