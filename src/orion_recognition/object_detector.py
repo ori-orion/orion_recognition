de1645a09c587c59393608f5ca463f4d3fab000e
Ricardo Cannizzaro <ricardo.cannizzaro@gmail.com>
2023-06-28 15:36:55 +0100

    Stopping using the classifier.

--- src/orion_recognition/object_detector.py
+++ src/orion_recognition/object_detector.py
@@ -36,7 +36,7 @@ torch.hub.set_dir(data_path)
 
 
 class ObjectDetector(torch.nn.Module):
-    def __init__(self, algorithm="yolotrt"):
+    def __init__(self, algorithm="yolo"):
         """
         :param algorithm: 'yolo' or 'yolotrt'
         """
